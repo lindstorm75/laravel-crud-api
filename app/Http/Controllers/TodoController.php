2fa45d9186ddb236a5a9b49b82cdf3cab436f6c0
Thanapong Angkha <lindstorm76@outlook.com>
2021-04-02 23:07:09 +0700

    Added CRUD operations

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Todo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "title" => "required|string",
+            "detail" => "required|string",
+        ]);
+        return Todo::create([
+            "title" => $request["title"],
+            "detail" => $request["detail"],
+            "completed" => $request["completed"] ? $request["completed"] : false
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Todo::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Finding it first so that you can return the updated
+        // version back to the user.
+        $todo = Todo::find($id);
+        $todo->update($request->all());
+        return $todo;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Todo::destroy($id);
+        return [
+            "message" => "Todo removed!"
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/todos", [TodoController::class, "index"]);
+Route::get("/todos/{id}", [TodoController::class, "show"]);
+Route::post("/todos", [TodoController::class, "store"]);
+Route::put("/todos/{id}", [TodoController::class, "update"]);
+Route::delete("/todos/{id}", [TodoController::class, "destroy"]);
